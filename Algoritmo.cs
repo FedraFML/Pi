69254dd6eab46f19c9696560d3ee028958fe8a84
FedraFML <a00835797@tec.mx>
2024-10-17 05:43:54 -0600

    Add project files.

---
+++ Algoritmo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    internal class Algoritmo
+    {
+        public static double CalcularPi(int n)
+        {
+            double pi = 0;
+            for (int i = 0; i < n; i++)
+            {
+                pi += Math.Pow(-1, i) / (2 * i + 1);
+            }
+            return pi * 4;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Recibir el valor de n
+            if (int.TryParse(textBox1.Text, out int n))
+            {
+                // Calcular el valor de pi
+                double pi = Algoritmo.CalcularPi(n);
+                // Mostrar el valor de pi
+                label1.Text = pi.ToString();
+                // Llenar el DataGridView
+                DataTable tabla = new DataTable();
+                DataColumn columna = new DataColumn("X");
+                DataColumn columna2 = new DataColumn("Y");
+                tabla.Columns.Add(columna);
+                tabla.Columns.Add(columna2);
+
+            }
+            else
+            {
+                // Handle invalid input
+                label1.Text = "Invalid input";
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Crear una tabla
+            DataTable tabla = new DataTable();
+            // Crear una columna
+            DataColumn columna = new DataColumn("X");
+            DataColumn columna2 = new DataColumn("Y");
+            // Agregar la columna a la tabla
+            tabla.Columns.Add(columna);
+            tabla.Columns.Add(columna2);
+            // Agregar filas a la tabla
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow fila = tabla.NewRow();
+                fila["X"] = i;
+                fila["Y"] = Math.Pow(i, 2);
+                tabla.Rows.Add(fila);
+            }
+
+            // Mostrar la tabla en el DataGridView
+            dataGridView1.DataSource = tabla;
+
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Llenar el DataGridView
+            DataTable tabla = new DataTable();
+            DataColumn columna = new DataColumn("X");
+            DataColumn columna2 = new DataColumn("Y");
+            tabla.Columns.Add(columna);
+            tabla.Columns.Add(columna2);
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow fila = tabla.NewRow();
+                fila["X"] = i;
+                fila["Y"] = Math.Pow(i, 2);
+                tabla.Rows.Add(fila);
+            }
+            dataGridView1.DataSource = tabla;
+
+
+        }
+    }
+}
